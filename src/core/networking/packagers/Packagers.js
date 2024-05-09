e76b3a282b7119fbc6d2c7177aa249e27ba0cf38
Willian Marques <willian.goncalves4@gmail.com>
2024-05-09 10:35:52 -0300

    chore: removing unused files

--- src/core/networking/packagers/Packagers.js
+++
@@ -1,4 +0,0 @@
-import ConnectionStatePackager from './ConnectionStatePackager.js';
-import HandshakePackager from './HandshakePackager.js';
-
-export default () => [new ConnectionStatePackager(), new HandshakePackager()];
